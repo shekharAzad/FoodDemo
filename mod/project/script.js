ee447a30fb503ce61f7e26a6a7e879ae23cde757
Chandra Shekhar <114468403+shekharAzad@users.noreply.github.com>
2022-10-14 10:01:34 +0530

    Add files via upload

---
+++ mod/project/script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+const order = document.querySelector(".item");
+const button = document.querySelector("button");
+const h4 = document.querySelector(".h4");
+button.addEventListener("click", function () {
+  // order.innerHTML
+  console.log(order.innerhtml);
+});
